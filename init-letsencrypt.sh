20d259808a39ba83e4b57882104cf9cb4c88fb46
ZacRC <1234rack1234@gmail.com>
2024-11-28 02:06:49 -0600

    qw

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,35 +5,28 @@ rsa_key_size=4096
 data_path="./certbot"
 email="zacharyrcherny@gmail.com"
 
-# Stop existing containers
+# Stop existing containers and clean up
 docker compose down
+rm -rf "$data_path"
 
 # Create required directories
 mkdir -p "$data_path/conf/live/solforge.live"
 mkdir -p "$data_path/www"
 
-if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
-    echo "### Downloading recommended TLS parameters ..."
-    curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-    curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
-fi
+# Download TLS parameters
+echo "### Downloading recommended TLS parameters ..."
+curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
 
-# Create dummy certificate
-echo "### Creating dummy certificate ..."
-openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1 \
-    -keyout "$data_path/conf/live/solforge.live/privkey.pem" \
-    -out "$data_path/conf/live/solforge.live/fullchain.pem" \
-    -subj '/CN=localhost'
+# Start services
+echo "### Starting services ..."
+docker compose up -d
 
-# Start nginx
-echo "### Starting nginx ..."
-docker compose up -d nginx
+# Wait for services to start
+echo "### Waiting for services to start ..."
+sleep 30
 
-# Wait for nginx to start
-echo "### Waiting for nginx to start ..."
-sleep 10
-
-# Request Let's Encrypt certificate
+# Request certificate
 echo "### Requesting Let's Encrypt certificate ..."
 docker compose run --rm --entrypoint "\
     certbot certonly --webroot -w /var/www/certbot \
@@ -42,7 +35,21 @@ docker compose run --rm --entrypoint "\
     --agree-tos \
     --force-renewal \
     --non-interactive \
-    -d solforge.live -d www.solforge.live" certbot
+    -d solforge.live -d www.solforge.live \
+    --staging" certbot
+
+# If staging was successful, get real certificate
+if [ $? -eq 0 ]; then
+    echo "### Staging successful, getting real certificate ..."
+    docker compose run --rm --entrypoint "\
+        certbot certonly --webroot -w /var/www/certbot \
+        --email $email \
+        --rsa-key-size $rsa_key_size \
+        --agree-tos \
+        --force-renewal \
+        --non-interactive \
+        -d solforge.live -d www.solforge.live" certbot
+fi
 
 # Reload nginx
 echo "### Reloading nginx ..."
