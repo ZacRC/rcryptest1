0072da0b5b8baa22e96677cca9b001f3008a22aa
ZacRC <1234rack1234@gmail.com>
2024-12-02 17:07:51 -0600

    qawf

--- myproject/settings.py
+++ myproject/settings.py
@@ -129,6 +129,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'mainapp/static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
